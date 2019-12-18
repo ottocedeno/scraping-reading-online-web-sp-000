00973c81cbd7dc55f656345aeeca0d840b6d0ce9
Otto Cedeno <otto@ottostacos.com>
2019-12-18 00:40:47 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,12 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+
+css = doc.css('#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .title-oE5vT4')
+
+test = css.collect {|el| el.text}
+binding.pry
